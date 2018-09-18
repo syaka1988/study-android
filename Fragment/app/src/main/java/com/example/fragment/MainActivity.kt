babd38b028c626426444ed1b7c6e8868fdedb300
sayaka.kita <sayaka.kita.1988@gmail.com>
2018-09-19 00:33:12 +0900

    Fragmentプロジェクト完成

---
+++ Fragment/app/src/main/java/com/example/fragment/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.fragment
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity(), ButtonFragment.OnButtonClickListener {
+    override fun onButtonClicked() {
+        val fragment = supportFragmentManager.findFragmentByTag("labelFragment") as LabelFragment
+        fragment.update()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (supportFragmentManager.findFragmentByTag("labelFragment") == null) {
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.container, newLabelFragment(0), "labelFragment")
+                    .commit()
+        }
+    }
+}
